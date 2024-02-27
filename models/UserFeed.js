ce38585b947a14eb83b3394aaa952d5e4722a78c
Isaac J <duckhunterjr@gmail.com>
2024-02-26 19:55:51 -0800

    add sub schemas for collections

--- models/UserFeed.js
+++ models/UserFeed.js
@@ -11,56 +11,72 @@ connect(connectionString, {
     .then(() => console.log('connected to mongoDB'))
     .catch(err => console.log("cant connect to mongDB: ", err));
 
-// Schema for data 
+// Schemas for data
+const HomeStatusSchema = new Schema({
+    home_type: String,
+    mortgage: Number,
+});
+
+const OccupationSchema = new Schema({
+    occ_name: String,
+    occ_desc: String,
+});
+
+const IncomeSchema = new Schema({
+    income_level: String,
+    income_desc: String,
+});
+
+const EducationSchema = new Schema({
+    edu_level: String,
+    edu_desc: String,
+    alma_mater: String,
+});
+
+const AffiliationSchema = new Schema({
+    affiliation_name: String,
+    affiliation_desc: String,
+    affiliation_intensity: String,
+    affiliation_capture: Date,
+    captured_from: String,
+});
+
+const SocialViewSchema = new Schema({
+    view_name: String,
+    view_desc: String,
+    view_intensity: String,
+    view_capture: Date,
+    captured_from: String,
+});
+
+const PostSchema = new Schema({
+    post_title: String,
+    post_content: String,
+    post_capture: Date,
+    post_client: String,
+    hashtags: [String],
+});
+
+const SocialMediaSchema = new Schema({
+    platform: String,
+    handle: String,
+    SMID: String,
+});
+
 const UserFeedSchema = new Schema({
     iduser: { type: Number, required: true },
     f_name: { type: String },
     l_name: { type: String },
     DOB: { type: Date },
     gender: { type: String },
-    homeStatus: {
-        home_type: String,
-        mortgage: Number,
-    },
-    occupation: {
-        occ_name: String,
-        occ_desc: String,
-    },
-    income: {
-        income_level: String,
-        income_desc: String,
-    },
-    education: {
-        edu_level: String,
-        edu_desc: String,
-        alma_mater: String,
-    },
-    affiliations: [{
-        affiliation_name: String,
-        affiliation_desc: String,
-        affiliation_intensity: String,
-        affiliation_capture: Date,
-        captured_from: String,
-    }],
-    socialViews: [{
-        view_name: String,
-        view_desc: String,
-        view_intensity: String,
-        view_capture: Date,
-        captured_from: String,
-    }],
-    socialMedia: [{
-        platform: String,
-        handle: String,
-        SMID: String,
-    }],
-    posts: [{
-        post_title: String,
-        post_content: String,
-        post_capture: Date,
-        post_client: String,
-        hashtags: [String],
-    }],
+    homeStatus: HomeStatusSchema,
+    occupation: OccupationSchema,
+    income: IncomeSchema,
+    education: EducationSchema,
+    affiliations: [AffiliationSchema],
+    socialViews: [SocialViewSchema],
+    socialMedia: [SocialMediaSchema],
+    posts: [PostSchema],
 }, {
     timestamps: true
 });
